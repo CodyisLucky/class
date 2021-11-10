787c1c07a0c1d5eacec45bd8700540354aa588ba
Cody Petrick <codyislucky@gmail.com>
2021-11-09 19:16:36 -0800

    Finished MobilePhone challenge

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Printer printer = new Printer(50, true);
-        System.out.println(printer.addToner(50));
-        System.out.println("Initial page count = " + printer.getPagesPrinted());
-        int pagesPrinted = printer.printPages(4);
-        System.out.println("Pages printed was " + pagesPrinted + " new total print count is " + printer.getPagesPrinted());
-        pagesPrinted = printer.printPages(2);
-        System.out.println("Pages printed was " + pagesPrinted + " new total print count is " + printer.getPagesPrinted());
+        MobilePhone phone = new MobilePhone("1115556666");
+        Contact contact1 = new Contact("Jeff", "2255");
+        Contact contact2 = new Contact("Susy", "2266");
+        Contact contact3 = new Contact("Jeff", "2255");
+        Contact contact4 = new Contact("Jim", "2155");
+
+        System.out.println(phone.addNewContact(contact1));
+        System.out.println(phone.addNewContact(contact2));
+        System.out.println(phone.addNewContact(contact3));
+        System.out.println(phone.addNewContact(contact4));
     }
 
 
--- src/MobilePhone.java
+++ src/MobilePhone.java
@@ -1,2 +1,69 @@
+import java.util.ArrayList;
+
 public class MobilePhone {
+
+    private ArrayList<Contact> myContacts;
+    private String myNumber;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<>();
+    }
+
+    public boolean addNewContact(Contact contact){
+        if(findContact(contact) != -1){
+            return false;
+        }
+        else{
+            return myContacts.add(contact);
+        }
+    }
+
+    public boolean updateContact(Contact oldContact, Contact newContact){
+        int location = findContact(oldContact);
+        if(location != -1){
+            myContacts.set(location, newContact);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean removeContact(Contact contact){
+        int location = findContact(contact);
+        if(location != -1){
+            myContacts.remove(location);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    private int findContact(Contact contact){
+        return findContact(contact.getName()); //Need to check by name because even if the name and number of a different contact are the same, the contact is "different" in memory.
+    }
+
+    private int findContact(String contact){
+        for (int i = 0; i < myContacts.size(); i++){
+            if (myContacts.get(i).getName() == contact){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Contact queryContact(String contactName){
+        int index = findContact(contactName);
+        return myContacts.get(index);
+    }
+
+    public void printContacts(){
+        System.out.println("Contact List:");
+        for (int i = 0; i < myContacts.size(); i++){
+            System.out.println((i+1) + ". " + myContacts.get(i).getName() + " -> " + myContacts.get(i).getPhoneNumber());
+        }
+    }
 }
+
